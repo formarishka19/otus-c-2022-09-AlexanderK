5c4672474376bccb3b519ee4891f3f361bb72ed3
Александр Конобеев <formarishka19@yandex.ru>
2023-02-07 22:15:37 +0300

    allmost done

--- hw09_daemon/Readme.md
+++ hw09_daemon/Readme.md
@@ -6,5 +6,13 @@
 ## Компиляция и запуск (программа принимает единственный аргумент - в режиме демона)
 ```sh
 sh build.sh
-./daemon -d
+sudo ./daemon -d
 ```
+
+sudo nc -U /check_fsize.socket
+
+команды:
+check - проверка размера
+stop - стоп программы
+
+sudo tail -100 /var/log/syslog
--- hw09_daemon/main.c
+++ hw09_daemon/main.c
@@ -6,25 +6,63 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/un.h>
 #include <string.h>
 #include "libconfig.h"
 
-#define APP_NAME "FILE_SIZE_DAEMON"
+#define APP_NAME "FILE_DAEMON"
+#define U_SOCKET_NAME "check_fsize.socket"
+#define GREETING "Здравствуйте, введите команды check для получения размера файла или stop для завершения работы сервера\n"
+#define FILE_SIZE_MSG "Размер отслеживаемого файла (байт): "
+#define REPEATE_MSG "Получена неизвестная команда. Повторите ввод (ckeck или stop).\n"
+#define NOFILE_MSG "Не могу найти файл. Останавливаю сервер"
 
 
-void checkArgs(int *argc, char* argv[]) {
-    if (*argc != 2) {
-        printf("\n\n --- ! ОШИБКА ЗАПУСКА ----\n\n"
-        "Программа принимает на вход только один аргумент - демонизировать или нет\n\n"
-        "------ Синтаксис ------\n"
-        "%s -d\n"
-        "-----------------------\n\n"
-        "Пример запуска\n"
-        "%s -d\n\n"
-        "--- Повторите запуск правильно ---\n\n", argv[0], argv[0]);
-        exit (1);
+size_t getFileSize(char* filename) {
+    FILE *fpin;
+    if ((fpin=fopen(filename, "rb") ) == NULL) {
+        syslog(LOG_ERR, "Error opening file %s. Exiting...", filename);
+        return(0);
     }
+    struct stat finfo;
+    if (!fstat(fileno(fpin), &finfo)) {
+        fclose(fpin);
+        return finfo.st_size;
+    }
+    else {
+        printf("Cannot get file info.\n");
+        syslog(LOG_ERR, "Cannot get file info %s", filename);
+        return(0);
+    }
+}
+
+char* getFileName(char* config_file) {
+    config_t cfg;
+    const char* filename;
+    char* filename_res = (char*) malloc(100 * sizeof(char));
+
+    if(! config_read_file(&cfg, config_file))
+    {
+        syslog(LOG_ERR, "Ошибка чтения конфигурации %s:%d - %s", config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
+        config_destroy(&cfg);
+        free(filename_res);
+        exit(EXIT_FAILURE);
+    }
+    
+    if(config_lookup_string(&cfg, "file", &filename)) {
+        syslog(LOG_INFO, "Имя отслеживаемого файла: %s", filename);
+        strcpy(filename_res, filename);
+        config_destroy(&cfg);
+    }
+    else {
+        syslog(LOG_ERR, "Параметр 'file' не найден в конфигурации %s", config_file);
+        config_destroy(&cfg);
+        free(filename_res);
+        exit(EXIT_FAILURE);
+    }
+    return filename_res;
 }
 
 void daemonize(const char* cmd)
@@ -35,7 +73,6 @@ void daemonize(const char* cmd)
     struct sigaction    sa;
 
     openlog(cmd, LOG_CONS | LOG_PID, LOG_DAEMON);
-    // setlogmask(LOG_UPTO(LOG_DEBUG));
     syslog(LOG_INFO, "Начинаю демонизацию");
     umask(0);
     /*
@@ -97,53 +134,106 @@ void daemonize(const char* cmd)
     syslog(LOG_INFO, "Программа успешно демонизирована =)");
 }
 
-int main(int argc, char* argv[]) {
-
-    if (argc != 1 && argc != 2) {
-        (void)argc;
-        printf("Программа %s запущена с неверными аргументами. Выход.\n", APP_NAME);
+void socket_listen(char* filename) {
+    int sock, msgsock, rval; 
+    struct sockaddr_un server; 
+    char buf[100];
+    const char* greeting = GREETING;
+    const char* filesize = FILE_SIZE_MSG;
+    const char* repeate = REPEATE_MSG;
+    const char* nofile = NOFILE_MSG;
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0) {
+        syslog(LOG_ERR, "Невозможно открыть unix socket, выход.");
         exit(EXIT_FAILURE);
     }
+    server.sun_family = AF_UNIX;
+    strcpy(server.sun_path, U_SOCKET_NAME);
+    if (bind(sock, (struct sockaddr *) &server, sizeof(struct sockaddr_un))) {
+        syslog(LOG_ERR, "Невозможно присвоить адрес unix socket-у, выход.");
+        exit(1); 
+    }
+    syslog(LOG_INFO, "Адрес созданного сокета %s", server.sun_path);
+    listen(sock, 5);
+    for (;;) {
+        msgsock = accept(sock, 0, 0); 
+        if (msgsock == -1) {
+            syslog(LOG_ERR, "Невозможно открыть на сокете входящее соединение. Серверный сокет закрываем. Выход");
+            break; 
+        }
+        else {
+            send(msgsock, greeting, strlen(greeting), 0);
+            do {
+                bzero(buf, sizeof(buf));
+                if ((rval = read(msgsock, buf, 100)) < 0) {
+                    syslog(LOG_WARNING, "Невозможно прочитать входящее сообщение");
+                }
+                
+                else if (rval == 0) {
+                    syslog(LOG_ERR, "Ошибка соединения");
+                }
+                else {
 
-    // checkArgs(&argc, argv);
-    // string CUR_FILE_NAME = argv[1];
-    // printf("%d", argc);
-    config_t cfg;
-    const char *str;
-    if(! config_read_file(&cfg, "./cfg/daemon.cfg"))
-    {
-        fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),
-                config_error_line(&cfg), config_error_text(&cfg));
-        config_destroy(&cfg);
-        return(EXIT_FAILURE);
+                    if (!strncmp(buf, "stop", 4)) {
+                        syslog(LOG_INFO, "Получена команда остановки программы. Закрытие сокета %s", U_SOCKET_NAME);
+                        close(msgsock);
+                        close(sock);
+                        unlink(U_SOCKET_NAME);
+                        return;
+                    } else if (!strncmp(buf, "check", 5)) {
+                        syslog(LOG_INFO, "Получена запрос на проверку размера файла");
+                        size_t fsize = getFileSize(filename);
+                        if (fsize) {
+                            char str[10];
+                            sprintf(str, "%lu", fsize);
+                            send(msgsock, filesize, strlen(filesize), 0);
+                            send(msgsock, str, strlen(str), 0);
+                            send(msgsock, "\n", 1, 0);
+                            close(msgsock);
+                        }
+                        else {
+                            syslog(LOG_ERR, "Файл %s невозможно открыть/прочитать", filename);
+                            send(msgsock, nofile, strlen(nofile), 0);
+                            close(msgsock);
+                            close(sock);
+                            unlink(U_SOCKET_NAME);
+                            return;
+                        }
+                        
+                    } else {
+                        send(msgsock, repeate, strlen(repeate), 0); 
+                    }
+                }
+            } while (rval > 0);
+        }
+        close(msgsock);
     }
-    /* Get the file name. */
-    openlog(APP_NAME, LOG_CONS | LOG_PID, LOG_DAEMON);
-    if(config_lookup_string(&cfg, "file", &str)) {
-        syslog(LOG_INFO, "Имя отслеживаемого файла: %s\n", str);
-    }
-    else {
-        syslog(LOG_ERR, "Параметр 'file' не найден в конфигурации ./cfg/daemon.cfg\n");
-    }
-    config_destroy(&cfg);
+    close(sock);
+    unlink(U_SOCKET_NAME);
+}
 
-    if (argc == 2 && (strcmp(argv[1], "-d") == 0)) {
+int main(int argc, char* argv[]) {
+ 
+    if (argc == 2) {
         (void)argc;
+        argv[1] = "";
         daemonize(APP_NAME);
     }
     else if (argc == 1) {
+        (void)argc;
         printf("Программа %s была запущена в обычном режиме\n", APP_NAME);
+        printf("Имя сокета %s\n", U_SOCKET_NAME);
+        printf("nc -U %s\n", U_SOCKET_NAME);        
     }
     else {
-        printf("Программа %s запущена с неверными аргументами. Выход.\n", APP_NAME);
+        (void)argc;
+        printf("Программа %s запущена с неверным ключом. Выход.\n", APP_NAME);
         exit(EXIT_FAILURE);
     }
-    (void)argc;
-
     
-
-    sleep(60);
-    
-    
+    char* filename = getFileName("/home/otus/hw09_daemon/cfg/daemon.cfg");
+    syslog(LOG_INFO, "Открываем сокет для входящих запросов.");
+    socket_listen(filename);
+    syslog(LOG_INFO, "Завершение работы программы");
 	return 0;
 }
