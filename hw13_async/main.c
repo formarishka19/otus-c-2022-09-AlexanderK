9b5838aff337668b33e9dd5ac36520da586be69d
Александр Конобеев <formarishka19@yandex.ru>
2023-03-09 13:37:27 +0300

    after review

--- hw13_async/main.c
+++ hw13_async/main.c
@@ -13,11 +13,11 @@
 #include <time.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <pthread.h>
 
 
 #define RESPONSE_TEMPLATE "HTTP/1.1 %d %s\r\nDate: %s\r\nContent-Type: %s; charset=UTF-8\r\nServer: sasha/1.0\r\nContent-Length: %u\r\nConnection: close\r\n\r\n"
 #define RESPONSE_ERROR "HTTP/1.1 %d %s\r\nDate: %s\r\nConnection: close\r\n\r\n"
+#define CONTENT_TYPE "application/octet-stream"
 
 #define IPV4_ADDR_LEN 16
 #define MAX_EPOLL_EVENTS 128
@@ -44,39 +44,23 @@ enum RESPONE_STATUS {
     UNAVAILABLE = 500
 };
 
-typedef struct HTTP_HEADER {
-    char method[10];
-    char query[MAX_QUERY_LEN];
-    char version[10];
-} header;
-
 typedef struct HTTP_RESPONSE {
     unsigned int status;
-    char protocol[10];
-    char datetime[100];
-    char content_type[50];
-    char server[20];
+    char* datetime;
+    char* content_type;
     unsigned int content_length;
 } response;
 
 typedef struct SESSION {
     int fd;
     int status; //read/write
-    header http_h;
+    char query[MAX_QUERY_LEN];
 } session;
-
-typedef struct LOGDIR {
-    char dir_path[MAX_PATH_LEN];
-    int indexed;
-    GHashTable* filelist;
-} logdir;
 
 
 static char buffer[MAX_REQUEST_LEN];
 static struct epoll_event events[MAX_EPOLL_EVENTS];
-static session sessions[MAX_EPOLL_EVENTS];
-static logdir log_files;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+char directory[MAX_PATH_LEN];
 
 
 size_t getFileSize(char* filename) {
@@ -92,7 +76,7 @@ size_t getFileSize(char* filename) {
     }
 }
 
-void send_error(int* fd, response* resp) {
+void send_error(int fd, response* resp) {
     char status_verbose[15] = {0};
     switch (resp->status)
     {
@@ -115,7 +99,7 @@ void send_error(int* fd, response* resp) {
         return;
     }
     snprintf(headers, headers_len + 1, RESPONSE_ERROR, resp->status, status_verbose, resp->datetime);
-    send(*fd, headers, headers_len, MSG_DONTWAIT);
+    send(fd, headers, headers_len, MSG_DONTWAIT);
     free(headers);
     return;
 }
@@ -127,9 +111,7 @@ int parse_http(char* buffer, session* ses) {
     char* title = (char*)headers_split[0];
     title_split = g_strsplit((const gchar *)title, " ", -1);
     if ((char*)title_split[0] && (char*)title_split[1] && (char*)title_split[2]) {
-        strncpy(ses->http_h.method, (char*)title_split[0], 10);
-        strncpy(ses->http_h.query, &title_split[1][1], MAX_QUERY_LEN);
-        strncpy(ses->http_h.version, (char*)title_split[2], 10);
+        strncpy(ses->query, &title_split[1][1], MAX_QUERY_LEN);
     }
     else {
         puts("Invalid method or query or protocol in http request");
@@ -159,9 +141,9 @@ int setnonblocking(int sock)
  return 0;
 }
 
-int do_read(int fd, session* ses)
+int do_read(session* ses)
 {
-    int rc = recv(fd, buffer, sizeof(buffer), 0);
+    int rc = recv(ses->fd, buffer, sizeof(buffer), 0);
     if (rc < 0 || rc > MAX_REQUEST_LEN) {
         perror("read");
         return 1;
@@ -173,69 +155,76 @@ int do_read(int fd, session* ses)
     return 0;
 }
 
-int do_write(int fd, session* ses) // написать функцию формирующую респонз (передаем туда статус и структуру res)
-{
+int do_write(session* ses) {
     response res;
     int headers_len;
     char* file_data;
     char buf[100];
     char* filename;
     FILE *fp;
+
     time_t now = time(0);
     struct tm tm = *gmtime(&now);
-
+    res.datetime = malloc(sizeof(buf) * (sizeof(char) + 1));
     strftime(buf, sizeof(buf), "%a, %d %b %Y %H:%M:%S %Z", &tm);
     snprintf(res.datetime, sizeof(buf), "%s", buf);
 
-    int filename_len = snprintf(NULL, 0, "%s%s", log_files.dir_path, ses->http_h.query) + 1;
+    int filename_len = snprintf(NULL, 0, "%s%s", directory, ses->query) + 1;
     filename = malloc(filename_len);
     if(!filename) {
         res.status = UNAVAILABLE;
         puts("memory error while generating response");
-        send_error(&fd, &res);
+        send_error(ses->fd, &res);
+        free(res.datetime);
         return(EXIT_FAILURE);
     }
-    snprintf(filename, filename_len, "%s%s", log_files.dir_path, ses->http_h.query);
-    unsigned int fsize = (uint32_t)(uintptr_t)g_hash_table_lookup(log_files.filelist, filename);
+    snprintf(filename, filename_len, "%s%s", directory, ses->query);
+    size_t fsize = getFileSize(filename);
+
     if (!fsize) {
         printf("File %s not found\n", filename);
         res.status = NOT_FOUND;
-        send_error(&fd, &res);
+        send_error(ses->fd, &res);
         free(filename);
+        free(res.datetime);
         return(EXIT_FAILURE);
     }
     if ((fp=fopen(filename, "rb") ) == NULL) {
         printf("Error opening file %s\n", filename);
         res.status = FORBIDDEN;
-        send_error(&fd, &res);
+        send_error(ses->fd, &res);
         free(filename);
+        free(res.datetime);
         return(EXIT_FAILURE);
     }
 
-    strncpy(res.content_type, "application/octet-stream", 50);
+    res.content_type = malloc(strlen(CONTENT_TYPE) * (sizeof(char) + 1));
+    strcpy(res.content_type, CONTENT_TYPE);
+
     res.content_length = fsize;
+    res.status = OK;
     headers_len = snprintf(NULL, 0, RESPONSE_TEMPLATE, res.status, "OK", res.datetime, res.content_type, res.content_length);
     file_data = malloc(fsize * sizeof(char) + headers_len + 1);
     if (!file_data) {
         puts("memory error while reading file");
         free(filename);
+        free(res.content_type);
+        free(res.datetime);
         res.status = UNAVAILABLE;
-        send_error(&fd, &res);
+        send_error(ses->fd, &res);
         return(EXIT_FAILURE);
     }
+    
     snprintf(file_data, headers_len + 1, RESPONSE_TEMPLATE, res.status, "OK", res.datetime, res.content_type, res.content_length);
     size_t nbytes = fread(file_data + headers_len, sizeof(char), fsize, fp) + headers_len;
     fclose(fp);
-
-    res.status = OK;
     int offset = 0;
     int sent = 0;
     while (nbytes > 0) {
-        sent = send(fd, file_data + offset, nbytes, MSG_DONTWAIT);
+        sent = send(ses->fd, file_data + offset, nbytes, MSG_DONTWAIT);
         if (sent >= 0) {
             offset += sent;
             nbytes -= sent;
-            // printf("sent bytes %d\n", sent);
         }
         else if (errno == EAGAIN || errno == EWOULDBLOCK) {
             continue;
@@ -244,68 +233,21 @@ int do_write(int fd, session* ses) // написать функцию форми
             printf("errno %d while sending file\nreseting..\n", errno);
             free(filename);
             free(file_data);
+            free(res.content_type);
+            free(res.datetime);
             return(EXIT_FAILURE);
         }
     }
     free(filename);
     free(file_data);
+    free(res.content_type);
+    free(res.datetime);
     return(EXIT_SUCCESS);
 }
 
 void process_error(int fd)
 {
     printf("fd %d error!\n", fd);
-}
-
-void* get_files() {
-
-    DIR *dp;
-    struct dirent *ep;
-    char* dirfile;
-    unsigned int fsize;
-
-
-    while (1) {
-        pthread_mutex_lock(&mutex);
-        dp = opendir(log_files.dir_path);
-        if (!dp)
-        {
-            printf("Couldn't open the directory %s\n", log_files.dir_path);
-            exit(EXIT_FAILURE);
-        }
-        while ((ep = readdir(dp)) != NULL) {
-            if (strcmp(ep->d_name, ".") != 0 && strcmp(ep->d_name, "..") != 0) {
-                int dirfile_len = snprintf(NULL, 0, "%s%s", log_files.dir_path, ep->d_name) + 1;
-                dirfile = malloc(dirfile_len);
-                if(!dirfile) {
-                    puts("memory error while indexing folder");
-                    exit(EXIT_FAILURE);
-                }
-                snprintf(dirfile, dirfile_len, "%s%s", log_files.dir_path, ep->d_name);
-                fsize = (unsigned int) getFileSize(dirfile);
-                if (fsize == 0) {
-                    // printf("Размер проверяемого файла %s 0. Пропускаем его.\n", dirfile);
-                    free(dirfile);
-                    continue;
-                }
-                else {
-                    g_hash_table_insert(log_files.filelist, (gpointer)g_strdup(dirfile), (gpointer)(uintptr_t)fsize);
-                }
-                free(dirfile);
-            }
-        }
-        (void)closedir(dp);
-        log_files.indexed = 1;
-        pthread_mutex_unlock(&mutex);
-        // printf("directory indexed, next indexing in %d sec\n", DIR_REINDEXING_RATE);
-        // sleep(DIR_REINDEXING_RATE);
-    }
-
-    pthread_exit(NULL);
-}
-
-void clear_key(gpointer data) {
-    free(data);
 }
 
 int main(int argc, char** argv) {
@@ -329,14 +271,7 @@ int main(int argc, char** argv) {
     }
     g_strfreev(arg_split);
 
-
-    log_files.indexed = 0;
-    log_files.filelist = g_hash_table_new_full(g_str_hash, g_str_equal, clear_key, NULL);
-    strncpy(log_files.dir_path, argv[1], MAX_PATH_LEN);
-
-    pthread_t index_pid;
-    pthread_create(&index_pid, NULL, get_files, NULL);
-    pthread_detach(index_pid);
+    strncpy(directory, argv[1], MAX_PATH_LEN);
 
     port = strtol(temp, &p, 10);
     if (*p) {
@@ -381,13 +316,9 @@ int main(int argc, char** argv) {
 
     struct epoll_event connev;
     int events_count = 1;
-    while (!log_files.indexed) {
-        sleep(1);
-    }
 
     for (;;) {
         int nfds = epoll_wait(efd, events, MAX_EPOLL_EVENTS, -1);
-
         for (int i = 0; i < nfds; i++) {
             if (events[i].data.fd == listenfd) {
                 int connfd = accept(listenfd, NULL, NULL);
@@ -401,54 +332,51 @@ int main(int argc, char** argv) {
                     continue;
                 }
                 setnonblocking(connfd);
-                connev.data.fd = connfd;
-                sessions[connfd].fd = connfd;
-                sessions[connfd].status = READ;
+                session* ses = malloc(sizeof(session));
+                ses->status = READ;
+                ses->fd = connfd;
+                connev.data.ptr = ses;
                 connev.events = EPOLLIN | EPOLLOUT | EPOLLET | EPOLLRDHUP;
                 if (epoll_ctl(efd, EPOLL_CTL_ADD, connfd, &connev) < 0) {
                     perror("epoll_ctl");
-                    sessions[connfd].status = READ;
+                    free(ses);
                     close(connfd);
                     continue;
                 }
                 events_count++;
             }
             else {
-                int fd = events[i].data.fd;
-
-                if ((events[i].events & EPOLLIN) && sessions[fd].status == READ) {
-                    if (do_read(fd, &sessions[fd])) {
-                        sessions[fd].status = READ;
+                session* sess = malloc(sizeof(session));
+                sess = events[i].data.ptr;
+                int fd = sess->fd;
+                if ((events[i].events & EPOLLIN) && sess->status == READ) {
+                    if (do_read(sess)) {
                         epoll_ctl(efd, EPOLL_CTL_DEL, fd, &connev);
+                        free(sess);
                         close(fd);
                     }
                     else {
-                        sessions[fd].status = WRITE;
+                        sess->status = WRITE;
                     }
 
                 }
-
-                if ((events[i].events & EPOLLOUT) && sessions[fd].status == WRITE) {
-                    if (do_write(fd, &sessions[fd])) {
+                if ((events[i].events & EPOLLOUT) && sess->status == WRITE) {
+                    if (do_write(sess)) {
                         printf("error while sending respone, closing socket %d\n", fd);
                     }
-                    sessions[fd].status = READ;
                     epoll_ctl(efd, EPOLL_CTL_DEL, fd, &connev);
+                    free(sess);
                     close(fd);
                 }
-
                 if (events[i].events & EPOLLRDHUP) {
                     process_error(fd);
-                    sessions[fd].status = READ;
                     epoll_ctl(efd, EPOLL_CTL_DEL, fd, &connev);
+                    free(sess);
                     close(fd);
                 }
                 events_count--;
-
             }
         }
     }
-    g_hash_table_destroy(log_files.filelist);
-    pthread_mutex_destroy(&mutex);
     return 0;
 }
